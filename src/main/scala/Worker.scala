c010b3d9cf6f69c58ea31a5110ea2c1f007c563d
Jakub Raczka <j.raczka@kainos.com>
2018-03-12 18:54:26 +0100

    changes

--- src/main/scala/Worker.scala
+++ src/main/scala/Worker.scala
@@ -2,9 +2,8 @@ import akka.actor.Actor
 
 class Worker extends Actor {
   override def receive: Receive = {
-    case StartTask(url) => {
+    case StartTask(url) =>
       val json = scala.io.Source.fromURL(url)
-      println(json)
-    }
+      println(json.mkString)
   }
 }
