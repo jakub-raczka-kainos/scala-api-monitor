25540c95f7a2336be351eb28bfbda00d9d4b719f
Jakub Raczka <j.raczka@kainos.com>
2018-03-13 15:28:31 +0100

    changes

--- build.sbt
+++ build.sbt
@@ -9,4 +9,9 @@ libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.0.11"
 libraryDependencies += "com.typesafe.akka" %% "akka-http-testkit" % "10.0.11" % "test"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
-        
+
+libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.3.0"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.11"
+
+libraryDependencies += "net.liftweb" %% "lift-json" % "3.2.0"
--- src/main/scala/Worker.scala
+++ src/main/scala/Worker.scala
@@ -1,9 +1,22 @@
 import akka.actor.Actor
+import net.liftweb.json.JInt
+import net.liftweb.json._
+
+import scalaj.http._
+import spray.json._
+import spray.json.DefaultJsonProtocol._
 
 class Worker extends Actor {
   override def receive: Receive = {
     case StartTask(url) =>
-      val json = scala.io.Source.fromURL(url)
-      println(json.mkString)
+      val content = scala.io.Source.fromURL(url)
+      val rawJson = Http(url).asString.body
+//      val allIds = 'lastUpdate / 'book / * / 'author
+//      println(rawJson)
+
+      val stops = parse(rawJson)
+        .children
+        .head \ "stops" \ "stopId" \ classOf[JInt]
+      stops.foreach(println)
   }
 }
