3f99edb23cef85f730d0ea4cd0b14c272df1f298
Kamil Koczkowski <kamilk@kainos.com>
2018-03-14 14:48:52 +0100

    Merged Response Json with the date,url wrapper.

--- src/main/scala/Worker.scala
+++ src/main/scala/Worker.scala
@@ -1,17 +1,26 @@
+import java.time.Instant
 import akka.actor.Actor
-
 import scalaj.http.Http
 
 class Worker extends Actor {
   override def receive: Receive = {
     case FetchDelays(url) =>
-      print('.')
       val id = url.split("=").last
-      try {
-        val rawJson = Http(url).asString.body
-        parse(rawJson)
+      val timeInMillis = System.currentTimeMillis()
+      val instant = Instant.ofEpochMilli(timeInMillis)
 
-        if (id == 35640 ) print(rawJson)
+      try {
+
+        val rawJson = Http(url).asString.body
+
+        val wrapper = s"""{
+          | "url":"${url}",
+          | "date":"${instant}",
+          | "content":${rawJson}
+         }""".stripMargin
+
+        println(wrapper)
+
       } catch {
         case e:java.net.SocketTimeoutException => println("BAD ID: " + id)
       }
