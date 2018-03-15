6db2476ec93378392d021c1c1bb3aa0226423a07
Jakub Raczka <j.raczka@kainos.com>
2018-03-15 14:48:47 +0100

    added external config

--- src/main/scala/Ztm.scala
+++ src/main/scala/Ztm.scala
@@ -1,26 +1,27 @@
 import akka.actor.{Actor, Props}
-import akka.routing.{BalancingPool, RoundRobinPool}
+import akka.routing.BalancingPool
 import net.liftweb.json.JInt
 import net.liftweb.json._
 
 import scalaj.http._
 
 class Ztm extends Actor {
-  val workerActors = context.actorOf(Props[Worker].withDispatcher("my-blocking-dispatcher").withRouter(BalancingPool(16)), name = "WorkerActors")
+
+  val workerActors = context.actorOf(Props[Worker]
+    .withDispatcher("my-blocking-dispatcher")
+    .withRouter(BalancingPool(16)), name = "WorkerActors")
 
   override def receive: Receive = {
     case StartTask(url) =>
       val rawJson = Http(url).asString.body
-
-
 
       val stops = parse(rawJson)
         .children
         .head \ "stops" \ "stopId" \ classOf[JInt]
 //      stops.foreach(println)
 //      println("Stops" + stops.length)
-
-      val delays_url_prefix = "http://87.98.237.99:88/delays?stopId="
+      import com.typesafe.config.ConfigFactory
+      val delays_url_prefix = ConfigFactory.load("application.conf").getString("urls.delays_url_prefix")
 
       stops.foreach( id => workerActors ! FetchDelays(delays_url_prefix + id))
   }
